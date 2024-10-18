47c6eb58b41923649583d2d1773e383e27ffd6ca
Jyotiska Saha <127967389+zico-saha@users.noreply.github.com>
2024-10-18 15:14:44 +0530

    Add files via upload

---
+++ Fenwick_Tree/src/FenwickTree.cpp
@@ -0,0 +1,35 @@
+#include "../header/FenwickTree.h"
+
+FenwickTree::FenwickTree(const vector<int> &arr)
+{
+    this->size = arr.size() + 1;
+    this->tree.resize(this->size, 0);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        pointUpdate(i, arr[i]);
+    }
+}
+
+void FenwickTree::pointUpdate(int index, int value)
+{
+    for (int i = (index + 1); i < this->size; i += (i & (-i)))
+    {
+        this->tree[i] += value;
+    }
+}
+
+int FenwickTree::prefixSum(int index) const
+{
+    int sum = 0;
+    for (int i = (index + 1); i > 0; i -= (i & (-i)))
+    {
+        sum += this->tree[i];
+    }
+    return sum;
+}
+
+int FenwickTree::querySum(int left, int right) const
+{
+    return (prefixSum(right) - prefixSum(left - 1));
+}
